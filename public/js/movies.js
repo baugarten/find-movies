1b2a85dfec398d5c4dd72674cf4dc75b766d218b
Ben Augarten <b.augarten@gmail.com>
2013-02-02 23:31:26 -0800

    Integrated with amazon and netflix

--- public/js/movies.js
+++ public/js/movies.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   });
 
   var publishedTime = 0,
-      movieData = [];
+      movieData = {};
 
 
   function search(value) {
@@ -33,34 +33,72 @@ $(document).ready(function() {
         date = new Date(),
         big = (movieList.length <= 4);
         imageFormat = big ? '600x600' : '225x225'; 
-    console.log(movieList[0]);
     movieList.forEach(function(movie) {
       if (publishedTime > date.getTime()) {
         display = false; 
         return;
       }
-      var cutdesc = movie.longDescription.substring(0, 210),
+      var movieStr = "",
+          cutdesc = (movie.longDescription) ? movie.longDescription.substring(0, 210) : "",
           desc = cutdesc.substring(0, cutdesc.lastIndexOf(' ')) + "<br />Read more...",
-          newMovie = {
-            title: movie.trackName,
-            date: movie.trackName.match(/\d{4}/) ? "" : '('+movie.releaseDate.substring(0, 4)+')',
-            description: desc,
-          };
+          newMovie = (movie.trackId in movieData) ? movieData[movie.trackId] :
+            {
+              id: movie.trackId,
+              title: movie.trackName,
+              date: movie.trackName.match(/\d{4}/) ? "" : '('+movie.releaseDate.substring(0, 4)+')',
+              description: desc,
+              netflix: false,
+              amazon: false,
+            };
       newMovie[imagekey(big)] = movie.artworkUrl100.replace(/100x100/, imageFormat);
-      movieData.push(newMovie);
-      var movieStr = movieTmpl
-        .replace(/%IMAGE_LINK%/g, newMovie[imagekey(big)]) 
-        .replace(/%TITLE%/, newMovie.title)
-        .replace(/\(%YEAR%\)/, newMovie.date)
-        .replace(/%DESCRIPTION%/, newMovie.description);
-      if (big) movieStr = movieStr.replace(/class=\'title\'/, "class='title big'");
-      if (big) movieStr = movieStr.replace(/class=\'movie\'/, "class='movie big'");
+      movieData[newMovie.id] = newMovie;
+      if (big) {
+        getInfo(newMovie);
+        movieStr = renderBig(newMovie);
+      } else {
+        movieStr = renderSmall(newMovie)
+      }
+
       movies = movies + movieStr;
     });
     if (display) { 
       publishedTime = date.getTime();
       $("#results").html(movies);
+      postRender();
     }
+  }
+
+  function renderBig(movie) {
+    var html = render(movie, true)
+      .replace(/%IMAGE_LINK%/g, movie[imagekey(true)]) 
+      .replace(/class=\'movie\'/, "class='movie big'")
+      .replace(/%BIG%.*?%BIG%/, '')
+      .replace(/%AMAZON\.PRICE%/, movie.amazon && movie.amazon.instant)
+      .replace(/%NETFLIX\.AVAILABLE%/, movie.netflix && movie.netflix.instant);
+    if (movie.amazon) {
+      html = html.replace(/%AMAZON%/g, '');
+      html = html.replace(/%AMAZON%/g, '');
+    } else {
+      html = html.replace(/%AMAZON%.*?%AMAZON%/, '');
+    }
+    console.log(movie.amazon.instant);
+    console.log(movie.netflix.instant);
+    return html 
+  }
+
+  function renderSmall(movie, big) {
+    big = !!big;
+    return movieStr = render(movie)
+      .replace(/%IMAGE_LINK%/g, movie[imagekey(false)]) 
+      .replace(/%BIG%/g, '');
+  }
+
+  function render(movie) {
+    return movieStr = movieTmpl
+      .replace(/%ID%/g, movie.id)
+      .replace(/%TITLE%/, movie.title)
+      .replace(/\(%YEAR%\)/, movie.date)
+      .replace(/%DESCRIPTION%/, movie.description)
   }
 
   function imagekey(big) {
@@ -68,15 +106,104 @@ $(document).ready(function() {
   }
 
   function getInfo(movie) {
+    netflixUpdate(movie);
+    amazonUpdate(movie);
+  }
 
+  function rerender(movie) {
+    if ($("#" + movie.id).length < 1 || !$("#" + movie.id).hasClass('big')) return;
+    var html = renderBig(movie);
+    $("#" + movie.id).replaceWith(html);
+  }
+
+  function postRender() {
+    /*var num = $(".movie").length;
+    console.log(num);
+    if (num < 4) {
+      $(".movie.big").css({
+        width: (100/num - 1) + "%"
+      });
+    }*/
+  }
+
+  function netflixUpdate(movie) {
+    if (movie.netflix) return;
+
+    $.ajax({
+      method: "GET",
+      url: "/netflix",
+      data: {
+        title: encodeURI(movie.title),
+        year: movie.date.substring(1, movie.date.length - 1),
+      },
+      success: function(data) {
+        if (data.d.results.length < 1) {
+          movie.netflixNotFound = true;
+          return;
+        }
+        var props = data.d.results[0];
+        movie.netflix = {
+          instant: props.Instant.Available,
+          url: props.Url
+        };
+        rerender(movie);
+      },
+      error: function(a, b, c) {
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      },
+    });
+  }
+  function amazonUpdate(movie) {
+    if (movie.amazon) return;
+    console.log("AMAZON UPDATE");
+    $.ajax({
+      method: 'GET',
+      url: '/amazon',
+      data: {
+        title: encodeURI(movie.title),
+        year: movie.date.substring(1, movie.date.length - 1),
+      },
+      success: function(data) {
+        console.log(data);
+        var items = data.ItemSearchResponse.Items.Item;
+        if (items.length < 0) {
+          movie.amazonNotFound = true;
+          return;
+        }
+        for (var i = 0; i < items.length; i++) {
+          var item = items[i];
+          if (item.ItemAttributes.ProductTypeName !== "DOWNLOADABLE_MOVIE") {
+            continue;
+          }
+          movie.amazon = {
+            instant: item.Offers.Offer.OfferListing.Price.FormattedPrice,
+            url: item.DetailPageUrl,
+          }
+          console.log("UPDATED");
+          console.log(movie.amazon);
+          rerender(movie);
+        }
+      },
+      error: function(a, b, c) {
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      }
+    });
   }
 
   var movieTmpl = "\
-    <div class='movie'> \
+    <div class='movie' id='%ID%'> \
       <div class='title'>%TITLE% (%YEAR%)</div> \
       <!-- <div class='img' style=\"background-image:url('%IMAGE_LINK%')\"></div> --> \
       <img src='%IMAGE_LINK%' /> \
       <span class='description'>%DESCRIPTION%</span> \
+      <div %BIG% class='hidden' %BIG%> \
+        %AMAZON% <img src='/static/images/amazon_logo.jpg' /> %AMAZON.PRICE% %AMAZON%\
+        NETFLIX: %NETFLIX.AVAILABLE% \
+      </div> \
     </div> \
   ";
 
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-var express = require('express');
+var express = require('express'),
+    http = require('http'),
+    request = require('request'),
+    OperationHelper = require('apac').OperationHelper,
+    parser = require('xml2json');
 
 var app = express();
 
@@ -9,12 +13,47 @@ app.set('view engine', 'jade');
 
 app.use('/static', express.static(__dirname + "/public"));
 
+app.use(express.query());
+
+var opHelper = new OperationHelper({
+  awsId: process.env.AWSACCESSKEYID,
+  awsSecret: process.env.AWSSECRET, 
+  assocId: process.env.AWSASSOCIATETAG, 
+});
 
 app.configure('development', function(){
 });
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/netflix', function(req, res) {
+  request("http://odata.netflix.com/Catalog/Titles?$filter=Name eq '" + decodeURI(req.query.title) + "' and ReleaseYear eq " + req.query.year + "&$format=json", function(err, res2, body) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(body);
+    res.end();
+  });
+});
+
+app.get('/amazon', function(req, res) {
+  opHelper.execute('ItemSearch', {
+    'BrowseNode': '2858778011',
+    'SearchIndex': 'Video',
+    'Keywords': decodeURI(req.query.title),
+    //'Title': decodeURI(req.query.title),
+    'ResponseGroup': 'ItemAttributes,Offers'
+  }, function(error, results) {
+    if (error) {
+      res.writeHead(500);
+      res.write(error);
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(parser.toJson(results));
+      res.end();
+    }
+    
+  });
 });
 
 app.listen(process.env.PORT || 3000);
